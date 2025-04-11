00f2adcbd5fdea4f39fe6cb8fee35b98282c7c6b
stivn <wilstivenortega@gmail.com>
2025-04-11 08:52:52 -0500

    Atributos y constructor añadidos

--- src/Negocio/Banco.java
+++ src/Negocio/Banco.java
@@ -1,5 +1,18 @@
 package Negocio;
 
+import java.util.LinkedList;
+
 public class Banco {
+    /// Atributos
+    private String nombre;
+    private String direccion;
+    private LinkedList<Cuenta> listCuentas;
+
+    public Banco(String nombre, String direccion, LinkedList<Cuenta> listCuentas) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.listCuentas = new LinkedList<Cuenta>();
+    }
+
 
 }
